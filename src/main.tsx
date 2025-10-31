b8bab915a5888dd20610adaa180e42f6de46ba34
LENS1208 <kan.yamaji@gmail.com>
2025-10-31 11:54:15 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,37 @@ import "./index.css";
 import App from "./App";
 import "./scripts/migrate-demo-data";
 
+// Chart.js setup
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  TimeScale,
+} from 'chart.js';
+import 'chartjs-adapter-date-fns';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  PointElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  TimeScale
+);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
