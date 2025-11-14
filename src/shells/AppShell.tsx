a78b292dc18cd08c092e34bf98f6d2d560d89ae0
LENS1208 <kan.yamaji@gmail.com>
2025-11-14 14:52:11 +0900

    Updated AppShell.tsx

--- src/shells/AppShell.tsx
+++ src/shells/AppShell.tsx
@@ -377,8 +377,6 @@ function SideNav({ menu, activeKey, onUploadClick, logoImg, theme, toggleTheme }
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <circle cx="7" cy="15" r="5"></circle>
                     <circle cx="17" cy="15" r="5"></circle>
-                    <circle cx="7" cy="15" r="2.5"></circle>
-                    <circle cx="17" cy="15" r="2.5"></circle>
                     <line x1="12" y1="15" x2="12" y2="13"></line>
                     <line x1="7" y1="10" x2="7" y2="7"></line>
                     <line x1="17" y1="10" x2="17" y2="7"></line>
