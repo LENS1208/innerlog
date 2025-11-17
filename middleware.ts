84509dfc2b90ddb1567f65c50cd0433ad8d6656b
LENS1208 <kan.yamaji@gmail.com>
2025-11-17 22:46:42 +0900

    Added middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization');
+  const url = request.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'inner' && pwd === 'test') {
+      return NextResponse.next();
+    }
+  }
+
+  url.pathname = '/api/auth';
+
+  return NextResponse.rewrite(url, {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: '/((?!api/auth).*)',
+};
